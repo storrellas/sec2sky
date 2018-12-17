0107d17bb29b81b9e90ab8e8533d346190fdff51
storrellas <storrellas@gmail.com>
2018-12-17 13:43:34 +0000

    ENH: First commit

---
+++ api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Dectection(models.Model):
+    thread_id = models.IntegerField(null=True)
+    home_latitude = models.DecimalField(max_digits=5, decimal_places=2)
+    home_longitude = models.DecimalField(max_digits=5, decimal_places=2)
+    latitude = models.DecimalField(max_digits=5, decimal_places=2)
+    altitude = models.DecimalField(max_digits=5, decimal_places=2)
+    longitude = models.DecimalField(max_digits=5, decimal_places=2)
+    rssi = models.DecimalField(max_digits=5, decimal_places=2)
+    signal_type = models.CharField(max_length=500,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
