27b60939ba0f74f5986715b646aa4beaf2c651dd
storrellas <storrellas@gmail.com>
2018-12-17 14:23:12 +0000

    ENH: Adding fixtures

--- api/models.py
+++ api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+
 class Detection(models.Model):
+    description = models.CharField(max_length=500, null=True)
     thread_id = models.IntegerField(null=True)
     home_latitude = models.DecimalField(max_digits=5, decimal_places=2)
     home_longitude = models.DecimalField(max_digits=5, decimal_places=2)
@@ -8,6 +10,6 @@ class Detection(models.Model):
     altitude = models.DecimalField(max_digits=5, decimal_places=2)
     longitude = models.DecimalField(max_digits=5, decimal_places=2)
     rssi = models.DecimalField(max_digits=5, decimal_places=2)
-    signal_type = models.CharField(max_length=500,null=True)
+    signal_type = models.CharField(max_length=500, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
