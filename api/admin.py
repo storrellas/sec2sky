f4ec5f6e6dbc87296c10e685bfbbcfcc45454ff6
storrella <storrellas@gmail.com>
2018-12-27 01:03:54 +0100

    ENH: Created data model

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Detection
+from django.apps import apps
+from django.contrib.admin.sites import AlreadyRegistered
 
-admin.site.register(Detection)
+app_models = apps.get_app_config('api').get_models()
+for model in app_models:
+    try:
+        admin.site.register(model)
+    except AlreadyRegistered:
+        pass
+
+
+# admin.site.register(Detection)
+# admin.site.register(Detection)
--- api/models.py
+++ api/models.py
@@ -5,10 +5,71 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
+class Company(models.Model):
+    name = models.CharField(max_length=500, null=True)
+    description = models.CharField(max_length=500, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class SensorManager(models.Model):
+
+    VIEWER = 'VI'
+    OPERATOR = 'OP'
+    CONFIGURATOR = 'CO'
+    ADMINISTRATOR = 'AD'
+    EMPOWERED = 'EM'
+    S2S_MANAGER = 'SM'
+    S2S_TECHNICIAN = 'ST'
+    ROLE_CHOICES = (
+        (VIEWER, 'viewer'),
+        (OPERATOR, 'operator'),
+        (CONFIGURATOR, 'configurator'),
+        (ADMINISTRATOR, 'administrator'),
+        (EMPOWERED, 'empowered'),
+        (S2S_MANAGER, 's2s_manager'),
+        (S2S_TECHNICIAN, 's2s_technician'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    role = models.CharField(max_length=2, choices=ROLE_CHOICES, default=VIEWER,)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        SensorManager.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.sensormanager.save()
+
+class SensorGroup(models.Model):
+    name = models.CharField(max_length=500, null=True)
+    description = models.CharField(max_length=500, null=True)
+    managers = models.ManyToManyField(SensorManager, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Sensor(models.Model):
+    sensor_group = models.ForeignKey(SensorGroup, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=500, null=True)
+    description = models.CharField(max_length=500, null=True)
+    latitude = models.DecimalField(max_digits=5, decimal_places=2)
+    longitude = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
 
 class Detection(models.Model):
 
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, null=True)
     description = models.CharField(max_length=500, null=True)
     thread_id = models.IntegerField(null=True)
     home_latitude = models.DecimalField(max_digits=5, decimal_places=2)
