c05bfb2e986005493554fb12f3fb50cce3db344f
storrella <storrellas@gmail.com>
2018-12-31 16:14:36 +0100

    ENH: Detection List endpiont

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,7 @@ from rest_framework import routers
 from rest_framework.authtoken import views
 
 # Project imports
-from .views import SensorUserAPIView
+from .views import SensorUserAPIView, DetectionListAPIView
 from .views import SensorGroupViewSet, SensorViewSet
 #from .views import DetectionViewSet
 
@@ -20,6 +20,7 @@ router.register(r'sensor', SensorViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^user/', SensorUserAPIView.as_view()),
+    url('^sensor/(?P<sensor>.+)/detection/$', DetectionListAPIView.as_view()),
     url(r'^auth/', views.obtain_auth_token),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- api/views.py
+++ api/views.py
@@ -8,6 +8,7 @@ from rest_framework.authentication import SessionAuthentication, BasicAuthentica
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
 
 # Project
 from .serializers import DetectionSerializer, SensorUserSerializer
@@ -70,16 +71,13 @@ class SensorViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
 
-# class DetectionViewSet(viewsets.ModelViewSet):
-#     authentication_classes = (TokenAuthentication,)
-#     permission_classes = (IsAuthenticated,)
-#
-#
-#     model = Detection
-#     queryset = Detection.objects.all()
-#     serializer_class = DetectionSerializer
-#     renderer_classes = (JSONRenderer, )
-#
-#     def get_queryset(self):
-#         logger.info("Get Queryset for user '" + str(self.request.user) +  "'")
-#         return self.model.objects.filter(owner=self.request.user)
+class DetectionListAPIView(ListAPIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    serializer_class = DetectionSerializer
+    renderer_classes = (JSONRenderer, )
+
+
+    def get_queryset(self):
+        sensor = self.kwargs['sensor']
+        return Detection.objects.filter(sensor__pk=sensor)
