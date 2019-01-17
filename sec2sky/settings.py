6ea32994064cc50abe5709246cd45d195ab6219f
storrellas <storrellas@gmail.com>
2019-01-17 10:39:48 +0000

    ENH: Admin is also being proxied

--- sec2sky/settings.py
+++ sec2sky/settings.py
@@ -142,7 +142,7 @@ MQTT = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/admin/static/'
 
 # Import localsettings if any
 try:
