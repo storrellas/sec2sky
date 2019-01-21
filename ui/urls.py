22bb930edc21e7602bc101f9b7d52cb91d916191
storrellas <storrellas@gmail.com>
2019-01-21 15:30:59 +0000

    ENH: Login page

--- ui/urls.py
+++ ui/urls.py
@@ -5,4 +5,5 @@ from django.views.generic import TemplateView
 
 # Set URL patterns
 urlpatterns = []
+urlpatterns += url('^console/', TemplateView.as_view(template_name="dronetrap/console.html"), name='console'),
 urlpatterns += url('', TemplateView.as_view(template_name="registration/login.html"), name='index'),
