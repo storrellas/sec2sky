7756435506dd563fc289e91d09719f63d23421ae
storrellas <storrellas@gmail.com>
2019-01-22 08:49:37 +0000

    ÂENH: Generating UI

--- ui/urls.py
+++ ui/urls.py
@@ -6,4 +6,5 @@ from django.views.generic import TemplateView
 # Set URL patterns
 urlpatterns = []
 urlpatterns += url('^console/', TemplateView.as_view(template_name="dronetrap/base.html"), name='console'),
+urlpatterns += url('^test/', TemplateView.as_view(template_name="dronetrap/test.html"), name='console'),
 urlpatterns += url('', TemplateView.as_view(template_name="registration/login.html"), name='index'),
