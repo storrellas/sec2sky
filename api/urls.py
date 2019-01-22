886af57ad976a5d474c4d8b5001ed9d8b1d68c95
storrellas <storrellas@gmail.com>
2019-01-22 09:55:31 +0000

    ENH: Adding endpoints to verify token

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url, include
 # Restframework imports
 from rest_framework import routers
 from rest_framework.authtoken import views
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 # Project imports
 from .views import *
@@ -20,5 +20,6 @@ urlpatterns = [
     url('^test/mqtt/$', MQTTTestAPIView.as_view()),
     url(r'^token/$', TokenObtainPairView.as_view(), name='api-token'),
     url(r'^token/refresh/$', TokenRefreshView.as_view(), name='api-token-refresh'),
+    url(r'^token/verify/$', TokenVerifyView.as_view(), name='api-token-refresh'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
