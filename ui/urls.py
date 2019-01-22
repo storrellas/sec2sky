ebb6efb0cf9e15420cd31d94056a29c68d9be5c0
storrellas <storrellas@gmail.com>
2019-01-22 07:56:48 +0000

    ENH: First approach to console

--- ui/urls.py
+++ ui/urls.py
@@ -5,5 +5,5 @@ from django.views.generic import TemplateView
 
 # Set URL patterns
 urlpatterns = []
-urlpatterns += url('^console/', TemplateView.as_view(template_name="dronetrap/console.html"), name='console'),
+urlpatterns += url('^console/', TemplateView.as_view(template_name="dronetrap/base.html"), name='console'),
 urlpatterns += url('', TemplateView.as_view(template_name="registration/login.html"), name='index'),
