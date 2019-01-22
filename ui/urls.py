ca04570120e0476965efa5d1e1afd94471120b53
storrellas <storrellas@gmail.com>
2019-01-22 10:23:01 +0000

    ENH: Remove unused files

--- ui/urls.py
+++ ui/urls.py
@@ -5,6 +5,6 @@ from django.views.generic import TemplateView
 
 # Set URL patterns
 urlpatterns = []
-urlpatterns += url('^console/', TemplateView.as_view(template_name="dronetrap/console.html"), name='console'),
-urlpatterns += url('^test/', TemplateView.as_view(template_name="dronetrap/console_old.html"), name='console'),
+urlpatterns += url('^console', TemplateView.as_view(template_name="dronetrap/console.html"), name='console'),
+urlpatterns += url('^test', TemplateView.as_view(template_name="dronetrap/console_old.html"), name='console'),
 urlpatterns += url('', TemplateView.as_view(template_name="registration/login.html"), name='index'),
