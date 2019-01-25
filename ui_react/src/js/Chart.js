c1cc39932568ca6bd2ded44f23fec6c4c4a642a3
storrella <storrellas@gmail.com>
2019-01-26 00:04:57 +0100

    ENH: Cleaning code

--- ui_react/src/js/Chart.js
+++ ui_react/src/js/Chart.js
@@ -8,12 +8,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 
 import { mainListItems, secondaryListItems } from './listItems';
 import SimpleLineChart from './SimpleLineChart';
--- ui_react/src/js/Map.js
+++ ui_react/src/js/Map.js
@@ -3,24 +3,15 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
+
+// Map Imports
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 import GPX from 'gpx-for-runners';
 
-import { mainListItems, secondaryListItems } from './listItems';
-import SimpleLineChart from './SimpleLineChart';
-import SimpleTable from './SimpleTable';
+// Project imports
 import NavBar from './NavBar';
 import SideBar from './SideBar';
 
@@ -29,66 +20,9 @@ const Mapbox = ReactMapboxGl({
 });
 
 
-const drawerWidth = 240;
-
 const styles = theme => ({
   root: {
     display: 'flex',
-  },
-  toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
-  },
-  toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  title: {
-    flexGrow: 1,
-  },
-  drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing.unit * 7,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing.unit * 9,
-    },
   },
   appBarSpacer: theme.mixins.toolbar,
   content: {
@@ -97,24 +31,12 @@ const styles = theme => ({
     height: '100vh',
     overflow: 'auto',
   },
-  chartContainer: {
-    marginLeft: -22,
-  },
-  tableContainer: {
-    height: 320,
-  },
-  chartContainer: {
-    marginLeft: -22,
-  },
   mapContainer: {
     height: 320,
     width: 220,
   },
   input: {
     display: 'none'
-  },
-  h5: {
-    marginBottom: theme.spacing.unit * 2,
   },
 });
 
@@ -135,15 +57,15 @@ class Map extends React.Component {
   }
 
   handleOnReadEnd(){
-    const content = fileReader.result;
+    const file_content = fileReader.result;
   }
 
   handleInputFile(event){
     let fileReader = new FileReader()
     var obj = this;
     fileReader.onloadend = function(){
-        const content = fileReader.result;
-        const gpx = new GPX( content );
+        const file_content = fileReader.result;
+        const gpx = new GPX( file_content );
         obj.setState({
           route: gpx.trackpoints.map( trackpoint => [trackpoint.lon, trackpoint.lat])
         });
