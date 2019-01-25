b5c9c29a2c14151184dadd3338918ef75f631d58
storrella <storrellas@gmail.com>
2019-01-25 23:10:34 +0100

    ENH: Reorganising

--- ui_react/src/index.js
+++ ui_react/src/index.js
@@ -1,19 +1,7 @@
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./js/App.js";
-// import Dashboard from "./js/Dashboard.js";
-// import Login from "./js/Login.js";
-// //ReactDOM.render(<Login />, document.getElementById("root"));
-//
-//
-// ReactDOM.render(<Login />, document.getElementById("root"));
-
 import React from "react";
 import ReactDOM from "react-dom";
 import Login from "./js/Login.js";
-import Dashboard from "./js/Dashboard.js";
-import DashboardMap from "./js/DashboardMap.js";
+import Map from "./js/Map.js";
 import Chart from "./js/Chart.js";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
@@ -22,8 +10,8 @@ ReactDOM.render((
   <BrowserRouter>
     <div>
       <Route path="/" exact component={Login} />
-      <Route path="/map/" exact component={DashboardMap} />
+      <Route path="/map/" exact component={Map} />
       <Route path="/chart/" component={Chart} />
     </div>
   </BrowserRouter>
-), document.getElementById('root'))
+), document.getElementById('root')) 
