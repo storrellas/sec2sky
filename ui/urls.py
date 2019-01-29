cbe9fece17d3e8de5c2be6f0a750de4f487fe5cc
storrellas <storrellas@gmail.com>
2019-01-29 08:57:03 +0000

    ENH: Adding profile edition UI

--- ui/urls.py
+++ ui/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 
 # Set URL patterns
 urlpatterns = []
-urlpatterns += url('^console', TemplateView.as_view(template_name="dronetrap/console.html"), name='console'),
+urlpatterns += url('^console', TemplateView.as_view(template_name="dronetrap/console.html"), name='ui-console'),
+urlpatterns += url('^profile', TemplateView.as_view(template_name="dronetrap/profile.html"), name='ui-profile'),
 urlpatterns += url('^test', TemplateView.as_view(template_name="dronetrap/console_old.html"), name='console'),
-urlpatterns += url('', TemplateView.as_view(template_name="registration/login.html"), name='index'),
+urlpatterns += url('', TemplateView.as_view(template_name="registration/login.html"), name='ui-login'),
