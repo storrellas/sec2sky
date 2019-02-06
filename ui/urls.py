74209c33d5792c900af8e576c96a0ef206982e78
storrellas <storrellas@gmail.com>
2019-02-06 15:52:00 +0000

    ENH: Added SensorDetail

--- ui/urls.py
+++ ui/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import include, path
 
 from . import views
 from django.views.generic import TemplateView
@@ -10,6 +11,7 @@ urlpatterns += url('^profile', TemplateView.as_view(template_name="dronetrap/pro
 urlpatterns += url('^company', TemplateView.as_view(template_name="dronetrap/company.html"), name='ui-company'),
 urlpatterns += url('^user', TemplateView.as_view(template_name="dronetrap/user.html"), name='ui-user'),
 urlpatterns += url('^swarm', TemplateView.as_view(template_name="dronetrap/swarm.html"), name='ui-swarm'),
+urlpatterns += path('sensor/<int:id>/', TemplateView.as_view(template_name="dronetrap/sensor_detail.html"), name='ui-sensor-detail'),
 urlpatterns += url('^sensor', TemplateView.as_view(template_name="dronetrap/sensor.html"), name='ui-sensor'),
 urlpatterns += url('^test', TemplateView.as_view(template_name="dronetrap/test.html"), name='console'),
 urlpatterns += url('', TemplateView.as_view(template_name="registration/login.html"), name='ui-login'),
